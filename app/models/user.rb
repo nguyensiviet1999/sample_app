32309f9dc74a5e760ab8229a876834484f91af3c
nguyensiviet1999 <nguyensiviet1999@gmail.com>
2020-07-02 14:37:59 +0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
-    attr_accessor :name , :email
-    before_save { self.email = email.downcase }
-    validates :name, presence: true, length: { maximum: 50 } #validates(:name, presence: true)
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true,
-                        length: { maximum: 255 },
-                        format: { with: VALID_EMAIL_REGEX },
-                        uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+  # attr_accessor :name , :email
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 } #validates(:name, presence: true)
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
+
+# User.create(name: "Example User",email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   resources :users
-  get '/signup', to: 'users#new'
-  get '/help', to:'static_pages#help',as: 'helf'
-  get '/about', to:'static_pages#about'
-  get 'static_pages/contact'
+  get "/signup", to: "users#new"
+  get "/help", to: "static_pages#help", as: "helf"
+  get "/about", to: "static_pages#about"
+  get "static_pages/contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root "static_pages#home"
+  post "/signup", to: "users#create"
+  resources :users
 end
