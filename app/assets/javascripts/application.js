a4223e195726af808e71bbfb4327d3f8df543d3a
nguyensiviet1999 <nguyensiviet1999@gmail.com>
2020-07-02 17:46:45 +0700

    fix require jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,14 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import "bootstrap"
-//=require jquery
-//= require jquery_ujs 
-//= require bootstrap 
-//= require turbolinks 
-//= require_tree .
 import "../stylesheets/application"
+
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
