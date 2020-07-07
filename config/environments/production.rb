cb507791fc847f5842c12499d2e0ac74143e64c5
nguyensiviet1999 <nguyensiviet1999@gmail.com>
2020-07-07 13:31:40 +0700

    c

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  host = "viet-sample-app.herokuapp.com" #replace with your own url
+  host = "https://viet-sample-app.herokuapp.com" #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
