03f099dce7a88a5a6fa7979103f656f72f1e6514
nguyensiviet1999 <nguyensiviet1999@gmail.com>
2021-03-02 17:03:29 +0700

    Design comment and reaction completed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,15 +6,17 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import 'bootstrap/dist/js/bootstrap'
 
-import "jquery"
-require("jquery")
+
 require("bootstrap")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
 global.$ = jQuery;
+require("packs/comment")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
